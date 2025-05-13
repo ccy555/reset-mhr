bb8dc49f74932f2cfa952dc6c2b8f82c576c63d8
cy555 <2051279861@qq.com>
2025-05-13 09:25:28 +0800

    删除没用的信息

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import uuid
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 def start():
     # 生成一个随机的UUID
     unique_id = uuid.uuid4()
     return str(unique_id)
 
-# 调用start函数获取UUID
-generated_uuid = start()
-print(generated_uuid)
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
